48e8c22fda3971648743c9d451e5db39a0e38ac7
chaad98 <faritzchad@yahoo.com>
2024-06-18 02:56:16 +0800

    Added new dependecies, added products and users route, and added custom css

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,19 @@
 import { WithChildren } from "@/utils/types";
+import Sidebar from "../ui/dashboard/sidebar/sidebar";
+import Navbar from "../ui/dashboard/navbar/navbar";
 
 const Layout = ({ children }: WithChildren) => {
-  return <div>Layout</div>;
+  return (
+    <div>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        bg: "#151c2c",
+        bgSoft: "#182237",
+        text: "#ffffff",
+        textSoft: "#b7bac1",
+      },
     },
   },
   plugins: [],
