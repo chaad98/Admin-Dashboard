461c6e5fc968bb00b816d57ad7ef36e5b32dee02
chaad98 <faritzchad@yahoo.com>
2024-06-18 14:04:39 +0800

    Completed sidebar design

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,15 @@
 import { WithChildren } from "@/utils/types";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import Navbar from "../ui/dashboard/navbar/navbar";
+import styles from "../ui/dashboard/dashboard.module.css";
 
 const Layout = ({ children }: WithChildren) => {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.menu}>
         <Sidebar />
       </div>
-      <div>
+      <div className={styles.content}>
         <Navbar />
         {children}
       </div>
