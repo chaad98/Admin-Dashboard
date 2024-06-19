4e483dbecf7872359de2d16e260fa86f922bb88b
chaad98 <faritzchad@yahoo.com>
2024-06-20 01:37:28 +0800

    Completed users and products page design

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,5 +1,65 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import styles from "@/app/ui/dashboard/product/product.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href={"/dashboard/products/add"}>
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created at</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.product}>
+              <Image
+                src={"/noproduct.jpg"}
+                alt=""
+                width={40}
+                height={40}
+                className={styles.productImage}
+              />
+              Mineral water
+            </td>
+            <td>Clean water taken from top of the mountain</td>
+            <td>RM 2.00</td>
+            <td>20/6/2024</td>
+            <td>10</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={"/"}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href={""}>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,5 +1,65 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/user/user.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href={"/dashboard/users/add"}>
+          <button className={styles.addButton}>Add new</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created at</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.user}>
+              <Image
+                src={"/noavatar.png"}
+                alt=""
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+              Odar DaaS
+            </td>
+            <td>malaysia@odar.my</td>
+            <td>20/6/2024</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={"/"}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href={""}>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default UsersPage;
