9343b1a7720c822a5e61993d3f65225571f90ecb
chad <faritzchad@yahoo.com>
2024-06-20 17:51:12 +0800

    Completed single user page design

---
+++ src/app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,54 @@
+import styles from "@/app/ui/dashboard/user/singleUser/singleUser.module.css";
+import Image from "next/image";
+
+const SingleUser = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src={"/noavatar.png"} alt="" fill />
+        </div>
+        Odar DaaS
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Your username..." />
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Your email..." />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password..."
+          />
+          <label>Phone</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="Your phone number (+60123456789)..."
+          />
+          <label>Address</label>
+          <textarea
+            name="address"
+            id="address"
+            placeholder="Your address (Kuala Lumpur)..."
+          />
+          <label>Is Admin?</label>
+          <select name="isAdmin" id="isAdmin">
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+          <label>Is Active?</label>
+          <select name="isActive" id="isActive">
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+          <button className={styles.btnUpdate}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUser;
--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -42,7 +42,7 @@ const UsersPage = () => {
             <td>Active</td>
             <td>
               <div className={styles.buttons}>
-                <Link href={"/"}>
+                <Link href={"/dashboard/users/:userId"}>
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
