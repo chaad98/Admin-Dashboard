625248a1d77f522d04df6c6c0bd24a643c65ff61
chaad98 <faritzchad@yahoo.com>
2024-06-24 01:29:39 +0800

    Completed login page design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,16 @@
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
