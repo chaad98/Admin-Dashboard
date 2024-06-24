cbc6d786e12458026af2ddc12d9ac3d1791daece
chad <faritzchad@yahoo.com>
2024-06-24 15:49:01 +0800

    Modified logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 export function logger(message: string, error?: Error): void {
   if (process.env.NODE_ENV === "development") {
     if (error && error.message) {
-      console.error(error.message);
+      console.error(message, error.message);
     } else {
       console.log(message);
     }
