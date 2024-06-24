77edb5aa4b2f3e389b2b27da81bcd8b42efb1c1e
chaad98 <faritzchad@yahoo.com>
2024-06-25 01:46:55 +0800

    Added data arguments inside logger function

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,9 @@
-export function logger(message: string, error?: Error): void {
+export function logger(message: string, data: any, error?: Error): void {
   if (process.env.NODE_ENV === "development") {
     if (error && error.message) {
       console.error(message, error.message);
+    } else if (data !== undefined) {
+      console.log(message, data);
     } else {
       console.log(message);
     }
