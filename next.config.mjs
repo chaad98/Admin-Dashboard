818f5992bd564cda210548a052ab0bee8f587b23
chaad98 <faritzchad@yahoo.com>
2024-06-25 02:34:13 +0800

    Update to include image for runner profile picture

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.wired.com",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.wired.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
