348e953a16be33aa06f36fb52cef1808ecd9eac5
chad <faritzchad@yahoo.com>
2024-07-11 16:52:43 +0800

    Added user endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,2 +1,3 @@
 export const ORIGIN_ENDPOINT = process.env.NEXT_PUBLIC_ORIGIN_ENDPOINT || "";
 export const ADMIN_ENDPOINT = process.env.NEXT_PUBLIC_ADMIN_ENDPOINT || "";
+export const USER_ENDPOINT = process.env.NEXT_PUBLIC_USER_ENDPOINT || "";
