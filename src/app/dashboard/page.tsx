1da4d3d663d05cdd7dbee8ba50df63826543fc2f
chad <faritzchad@yahoo.com>
2024-07-15 17:20:20 +0800

    Added useEffect to check the state of token and user's data in useAuthStore

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
 import Card from "../ui/dashboard/card/card";
 import Chart from "../ui/dashboard/chart/chart";
 import styles from "../ui/dashboard/dashboard.module.css";
 import Rightbar from "../ui/dashboard/rightbar/rightbar";
 import Transaction from "../ui/dashboard/transaction/transaction";
+import useAuthStore from "@/store/useAuthStore";
+import { logger } from "@/utils/logger";
 
 const DashboardPage = () => {
+  const { token, user } = useAuthStore();
+  useEffect(() => {
+    logger("Token after login:", token);
+    logger("User after login:", user);
+  }, [token, user]);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
