84473ba35bd1d13eae0ad289218caa2c31d717d0
chad <faritzchad@yahoo.com>
2024-07-29 17:02:53 +0800

    Update to allow images to display within aws domain name

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.wired.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "media.wired.com",
-      },
-    ],
+    domains: ["mvig-daas.s3.ap-southeast-1.amazonaws.com"],
   },
 };
 
