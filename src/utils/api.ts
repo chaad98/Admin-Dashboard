12dbf864f7a0601e42dde1cbfe792077519488a3
chaad98 <faritzchad@yahoo.com>
2024-08-14 13:36:18 +0800

    Added new env endpoint and variable name as well

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,20 @@
 export const ORIGIN_ENDPOINT = process.env.NEXT_PUBLIC_ORIGIN_ENDPOINT || "";
-export const ADMIN_ENDPOINT = process.env.NEXT_PUBLIC_ADMIN_ENDPOINT || "";
+
+// web admin API
+export const ADMIN_USERS_ENDPOINT =
+  process.env.NEXT_PUBLIC_ADMIN_USERS_ENDPOINT || "";
+
+export const ADMIN_STORES_ENDPOINT =
+  process.env.NEXT_PUBLIC_ADMIN_STORES_ENDPOINT || "";
+
+export const ADMIN_PRODUCTS_ENDPOINT =
+  process.env.NEXT_PUBLIC_ADMIN_PRODUCTS_ENDPOINT || "";
+
+export const ADMIN_STATES_ENDPOINT =
+  process.env.NEXT_PUBLIC_ADMIN_STATES_ENDPOINT || "";
+
+export const ADMIN_DISTRICTS_ENDPOINT =
+  process.env.NEXT_PUBLIC_ADMIN_DISTRICTS_ENDPOINT || "";
+
+// react-native API
 export const USER_ENDPOINT = process.env.NEXT_PUBLIC_USER_ENDPOINT || "";
