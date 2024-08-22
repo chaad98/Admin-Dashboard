994c93c649f5cfe1415bb73e3cfe5dd3e747fab4
chaad98 <faritzchad@yahoo.com>
2024-08-22 15:02:15 +0800

    Refactored to userName instead for better readability

--- src/app/dashboard/users/[id]/page.tsx
+++ src/app/dashboard/users/[id]/page.tsx
@@ -79,10 +79,10 @@ const SingleUserPage = () => {
     }
   };
 
-  const singleStaff = async (token: any, userId: any) => {
+  const singleStaff = async (token: any, userName: any) => {
     try {
       setIsLoading(true);
-      const response = await viewStaffInfo(token, userId);
+      const response = await viewStaffInfo(token, userName);
       logger("Staff data:", response.data);
       setUser(response.data);
     } catch (error: any) {
