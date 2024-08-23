7f2b2472dda144a73a6d77d1e573bcc16fb965f6
Chad <faritzchad@yahoo.com>
2024-08-23 11:36:55 +0800

    Remove the newPath from the div

--- src/app/ui/dashboard/navbar/navbar.tsx
+++ src/app/ui/dashboard/navbar/navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>{newPath}</div>
+      <div className={styles.title}>{}</div>
       <div className={styles.menu}>
         <div className={styles.search}>
           <MdSearch />
